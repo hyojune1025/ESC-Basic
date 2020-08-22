fd70e2a9a323f1bbd402a4b181f8f93202179d37
2020060319 <hj102548@hanyang.ac.kr>
2020-08-22 13:57:23 +0900

    2일차

--- app/src/main/java/com/example/escbasicapp/MainActivity.java
+++ app/src/main/java/com/example/escbasicapp/MainActivity.java
@@ -2,13 +2,131 @@ package com.example.escbasicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton addContact;
+    private ImageButton contact;
+    private TextView phoneNum;
+    private TextView[] dials = new TextView[10];
+    private TextView star;
+    private TextView sharp;
+    private ImageButton message;
+    private ImageButton call;
+    private ImageButton backspace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpUI();
+    }
+
+    private void setUpUI() {
+        addContact = findViewById(R.id.main_ibtn_add); // 코드와 앱을 연결
+        contact = findViewById(R.id.main_ibtn_contact);
+        phoneNum = findViewById(R.id.main_tv_phone);
+
+        for (int i = 0; i < dials.length; i++) {
+            dials[i] = findViewById(getResourceID("main_tv_" + i, "id", this));
+        }
+
+        star = findViewById(R.id.main_tv_star);
+        sharp = findViewById(R.id.main_tv_sharp);
+        message = findViewById(R.id.main_ibtn_message);
+        call = findViewById(R.id.main_ibtn_call);
+        backspace = findViewById(R.id.main_ibtn_backspace);
+
+        addContact.setOnClickListener(new View.OnClickListener() { //공식처럼 외우기
+            @Override
+            public void onClick(View v) {
+                // TODO: 연락처 추가
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 연락처
+            }
+        });
+
+        setOnClickDial(star, "*");
+        setOnClickDial(sharp, "#");
+
+        for (int i = 0; i < dials.length; i++) {
+            setOnClickDial(dials[i], String.valueOf(i));
+        }
+
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 메시지
+            }
+        });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 전화
+            }
+        });
+
+        backspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(phoneNum.length() > 0) {
+                    phoneNum.setText(changeToDial(phoneNum.getText().subSequence(0, phoneNum.getText().length() - 1).toString()));
+                }
+            }
+        });
+    }
+
+    private void setOnClickDial(View view, final String input) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phoneNum.setText(changeToDial(phoneNum.getText() + input));
+            }
+        });
+    }
+
+    private int getResourceID(final String resName, final String resType, final Context ctx) {
+        final int ResourceID =
+                ctx.getResources().getIdentifier(resName, resType, ctx.getApplicationInfo().packageName);
+        if (ResourceID == 0) {
+            throw new IllegalArgumentException("No resource string found with name " + resName);
+        } else {
+            return ResourceID;
+        }
+    }
+
+    private String changeToDial(String phoneNum) {
+        phoneNum = phoneNum.replace("-", "");
+
+        if (phoneNum.contains("*") || phoneNum.contains("#")) {
+            return phoneNum;
+        } else {
+            if (phoneNum.length() >= 4 && phoneNum.length() <= 7) {
+                return phoneNum.subSequence(0, 3) + "-" + phoneNum.subSequence(3, phoneNum.length());
+            } else if (phoneNum.length() >= 8 && phoneNum.length() <= 11) {
+                return phoneNum.subSequence(0, 3) + "-" + phoneNum.subSequence(3, 7) + "-" +
+                        phoneNum.subSequence(7, phoneNum.length());
+            } else {
+                return phoneNum;
+            }
+        }
     }
 }
